de4041aef552699491a7ff08948e55ff4598492c
Ricardo <ricardcutzh@gmail.com>
2019-09-16 00:40:56 -0600

    arreglado el gitignore

--- src/Main/B2.java
+++ src/Main/B2.java
@@ -22,7 +22,7 @@ public class B2 extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Probando");
+        btn.setText("Que pex");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
