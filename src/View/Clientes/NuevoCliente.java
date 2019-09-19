04bffffd65760d57acf945fc76292b3765fcb581
Joseph <josephccaceres@gmail.com>
2019-09-19 11:32:08 -0600

    listar Clientes

---
+++ src/View/Clientes/NuevoCliente.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View.Clientes;
+
+import Main.B2;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Notebook
+ */
+public class NuevoCliente implements Initializable {
+
+    int id_Cliente = -1;
+
+    @FXML
+    private JFXTextField txtNombre;
+
+    @FXML
+    private JFXTextField txtDireccion;
+
+    @FXML
+    void clckAceptar(ActionEvent event) {
+
+        String nombre = txtNombre.getText();
+        String direccion = txtDireccion.getText();
+
+        if (this.id_Cliente == -1) {
+
+            if (insertar(nombre, direccion)) {
+
+                B2.GuiController.mensajeConsola("Cliente insertado exitosamente");
+            } else {
+
+                B2.GuiController.mensajeConsola("Ocurrió un error al insertar el Cliente");
+            }
+        } else {
+
+            if (editar(nombre, direccion)) {
+
+                B2.GuiController.mensajeConsola("Cliente actualizado exitosamente");
+            } else {
+
+                B2.GuiController.mensajeConsola("Ocurrió un error al actualizar el Cliente");
+            }
+
+        }
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    /*
+    +--------------------------------------
+    | Metodos de inserción o modificación
+    +---------------------------------------
+    
+     */
+    public boolean insertar(String nombre, String direccion) {
+
+        return true;
+    }
+
+    public boolean editar(String nombre, String direccion) {
+        return true;
+    }
+
+}
