7b3cc1ed996dc8308bd503b951a19cb4528fefb3
DanielGarcia097 <2009402600101@ingenieria.usac.edu.gt>
2019-09-23 12:22:05 -0600

    Script CreateDB y DropTablesDB
    Se crearon los archivos para crear las tablas en la base de datos y para eliminar las tablas en la base de datos.

---
+++ DB/DropTablesDB.sql
@@ -0,0 +1,14 @@
+--Eliminación de tablas de la base de datos--
+drop table agencia cascade constraints;
+drop table banco cascade constraints;
+drop table cliente cascade constraints;
+drop table cuenta cascade constraints;
+drop table mancomunada cascade constraints;
+drop table permiso cascade constraints;
+drop table rol_permiso cascade constraints;
+drop table rol cascade constraints;
+drop table terminal cascade constraints;
+drop table tipo_cuenta cascade constraints;
+drop table transaccion cascade constraints;
+drop table usuario cascade constraints;
+-----------------------------------------------
